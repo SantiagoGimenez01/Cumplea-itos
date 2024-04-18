0705bab1b43f2b342e49519ea8cc9fbe978aabae
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-04-18 18:00:51 -0300

    Modelado de datos y funciones

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,21 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+data Invitado = UnInvitado{
+    cansancio :: Number,
+    felicidad :: Number,
+    cancionFavorita :: String
+} deriving(Show, Eq)
+
+estaCansado :: Invitado -> Bool
+estaCansado invitado = cansancio invitado > 80
+
+seCansa :: Invitado -> Invitado
+seCansa invitado = UnInvitado{cansancio = cansancio invitado + 10}
+
+disfruta :: Invitado -> Invitado
+disfruta invitado = UnInvitado{felicidad = felicidad invitado + 100 - cansancio invitado}
+
+santi :: Invitado
+santi = UnInvitado{cansancio = 75, felicidad = 80, cancionFavorita = "Besame remix"}
