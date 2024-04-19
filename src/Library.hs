f31eda42b40ff5d54456bd349a0530a2fcde93c4
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-04-19 15:50:13 -0300

    Funciones probadas correctamente

--- src/Library.hs
+++ src/Library.hs
@@ -14,10 +14,10 @@ estaCansado :: Invitado -> Bool
 estaCansado invitado = cansancio invitado > 80
 
 seCansa :: Invitado -> Invitado
-seCansa invitado = UnInvitado{cansancio = cansancio invitado + 10}
+seCansa invitado = UnInvitado{felicidad = felicidad invitado, cansancio = cansancio invitado + 10, cancionFavorita = cancionFavorita invitado }
 
 disfruta :: Invitado -> Invitado
-disfruta invitado = UnInvitado{felicidad = felicidad invitado + 100 - cansancio invitado}
+disfruta invitado = UnInvitado{felicidad = felicidad invitado + 100 - cansancio invitado, cansancio = cansancio invitado, cancionFavorita = cancionFavorita invitado}
 
 santi :: Invitado
 santi = UnInvitado{cansancio = 75, felicidad = 80, cancionFavorita = "Besame remix"}
