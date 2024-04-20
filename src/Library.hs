72be499724b258131c27e48295bb663385c4d460
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-04-20 14:10:29 -0300

    Parte 3 terminada y probada correctamente

--- src/Library.hs
+++ src/Library.hs
@@ -12,6 +12,8 @@ data Invitado = UnInvitado{
 
 type Plancito = Invitado -> Invitado
 
+type Bandurria = [Invitado]
+
 estaCansado :: Invitado -> Bool
 estaCansado invitado = cansancio invitado > 80
 
@@ -23,6 +25,16 @@ disfruta invitado = UnInvitado{felicidad = felicidad invitado + 100 - cansancio 
 
 santi :: Invitado
 santi = UnInvitado{cansancio = 75, felicidad = 80, cancionFavorita = "Besame remix"}
+
+mili :: Invitado
+mili = UnInvitado{cansancio = 60, felicidad = 90, cancionFavorita = "Don't Blame Me"}
+
+male :: Invitado
+male = UnInvitado{cansancio = 82, felicidad = 100, cancionFavorita = "MP3"}
+
+paula :: Invitado
+paula = UnInvitado{cansancio = 85, felicidad = 100, cancionFavorita = "La Original"}
+
 
 unaCharlitaDeFulbo :: Plancito
 unaCharlitaDeFulbo = disfruta
@@ -38,3 +50,24 @@ tieneBuenGusto = even.length.cancionFavorita
 
 leVaADarFiaca :: Plancito -> Invitado -> Bool
 leVaADarFiaca plancito invitado = estaCansado(plancito invitado) 
+
+playlistDeBandurria :: Bandurria -> [String]
+playlistDeBandurria bandurria = map cancionFavorita bandurria
+
+sigueLaGira :: Bandurria -> Bandurria
+sigueLaGira bandurria = filter (not.estaCansado) bandurria 
+
+estadoDeBandurria:: Bandurria -> Plancito -> Bandurria
+estadoDeBandurria invitados plancito = map plancito invitados
+
+laRompe :: Bandurria -> Bool
+laRompe bandurria = all (not.estaCansado) bandurria
+
+suenaUnHitazo :: Bandurria -> Bool
+suenaUnHitazo bandurria = any tieneBuenGusto bandurria
+
+seArmaFieston :: Bandurria -> Bool
+seArmaFieston bandurria = sum (map felicidad bandurria) > 300
+
+seLaSubeABandurria :: Bandurria -> Plancito -> Bool
+seLaSubeABandurria bandurria plancito = seArmaFieston(estadoDeBandurria bandurria plancito)
